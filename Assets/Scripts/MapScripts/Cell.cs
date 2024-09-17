ebd92d905fdd31ee07a30479c61b987e0ec877a9
sigvaldar <58987007+sigvaldar@users.noreply.github.com>
2024-09-17 15:14:23 +0200

    update 17/09/2024
    upgrade map generator : fonctionnel

--- Assets/Scripts/MapScripts/Cell.cs
+++ Assets/Scripts/MapScripts/Cell.cs
@@ -13,3 +13,4 @@ public class Cell : MonoBehaviour
 
 }
 
+
--- Assets/Scripts/MapScripts/MapGenerator.cs
+++ Assets/Scripts/MapScripts/MapGenerator.cs
@@ -2,81 +2,166 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 //[ExecuteInEditMode]
 public class MapGenerator : MonoBehaviour
 {
-    [SerializeField] float GridHeight, GridWidth;
+    [SerializeField] int HexHeight, HexWidth;
     [SerializeField] GameObject CellPrefab, hexaGrid;
 
     [SerializeField] Texture2D MapTexture;
-    [SerializeField] int[,] MapTerrain,test;
-    int mapterraintype;
+    [SerializeField] int[,] MapTerrain;
 
     [SerializeField] List<Color> MapColors;
     [SerializeField] List<Material> CellMartial;
 
-    //[SerializeField] float CellSize = 1f;
-    //[SerializeField] Tilemap Tilemap;
-    //public GridLayout gridLayout;
-    //[SerializeField] private Grid grid;
     private void Awake()
     {
         GenerateMap();
         Debug.Log("map");
     }
     /// <summary>
-    /// Methode to create a 3d hexagonal map 
+    /// Methode to create a 3D hexagonal map with a Image 2D;
     /// </summary>
     void GenerateMap()
     {
-        ReadMap();
-        SetMapTab();
 
-        for (int i = 0; i < test.GetLength(0); i++)
+        // Taille de la carte en fonction des hexagones
+        int mapWidth = MapTexture.width / HexWidth;
+        int mapHeight = MapTexture.height / HexHeight;
+
+        MapTerrain = new int[mapWidth, mapHeight];
+
+        for (int x = 0; x < mapWidth; x++)
         {
-            for (int y = 0; y < test.GetLength(1); y++)
+            for (int y = 0; y < mapHeight; y++)
             {
-                SetCell(test[i,y]);
+                Vector2 hexCenter = GetHexCenter(x, y);
+                MapTerrain[x, y] = GetHexagonTerrainType((int)hexCenter.x, (int)hexCenter.y);
             }
         }
 
-        //for (int y = 0; y < GridHeight; y++)
-        //{
-        //    for (int x = 0; x < GridWidth; x++)
-        //    {
-        //        Vector3 position = HexPositioning(x, y, CellSize);
-        //        GameObject mCell = Instantiate(CellPrefab, position, Quaternion.identity);
-        //        mCell.transform.parent = hexaGrid.transform;
-        //    }
-        //}
+        SetMap();
     }
 
-    private void ReadMap()
+    private void SetMap()
     {
-        MapTerrain = new int[MapTexture.width, MapTexture.height];
-        for (int x = 0; x < MapTexture.width; x ++)
+        float CellSize = 1;//HexWidth * 0.5f;
+        for (int i = 0; i < MapTerrain.GetLength(0); i++)
         {
-            for (int y = 0; y < MapTexture.height; y ++)
+            for (int y = 0; y < MapTerrain.GetLength(1); y++)
             {
-                Color pixelColor = MapTexture.GetPixel(x, y);
-                MapTerrain[x, y] = GetTerrainTypeFromColor(pixelColor);
-                Debug.Log(MapTerrain[x,y]);
+                Vector3 position = HexPositioning(i, y, CellSize);
+                GameObject mCell = Instantiate(CellPrefab, position, Quaternion.identity);
+                mCell.transform.parent = hexaGrid.transform;
+
+                int terrainType = MapTerrain[i, y];
+                if (terrainType >= 0 && terrainType < CellMartial.Count)
+                {
+                    Renderer cellRenderer = mCell.GetComponent<Renderer>();
+                    if (cellRenderer != null)
+                    {
+                        cellRenderer.material = CellMartial[terrainType];
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"Terrain inconnu à la position ({i}, {y})");
+                }
+                //SetCell(MapTerrain[i, y],i,y);
             }
         }
     }
-
-    private void SetMapTab()
+    private void instantiateCell(int x, int y)
     {
-        test = DownsampleTerrainMap(MapTerrain,10);
+        //Vector3 position = HexPositioning(x, y, CellSize);
+        //GameObject mCell = Instantiate(CellPrefab, position, Quaternion.identity);
+        //mCell.transform.parent = hexaGrid.transform;
+    }
+    /// <summary>
+    /// Calcule la position du centre d'un hexagone dans l'image
+    /// </summary>
+    /// <param name="hexX"></param>
+    /// <param name="hexY"></param>
+    /// <returns></returns>
+    Vector2 GetHexCenter(int hexX, int hexY)
+    {
+        float offsetX = (hexY % 2 == 0) ? 0 : HexWidth / 2; // Décalage entre lignes d'hexagones
+        float centerX = hexX * HexWidth + offsetX + HexWidth / 2;
+        float centerY = hexY * (HexHeight * 0.75f) + HexHeight / 2;
+        return new Vector2(centerX, centerY);
     }
 
-    private void SetCell(int typeOfCell)
+    /// <summary>
+    /// Détermine le type de terrain en fonction de la couleur autour du centre d'un hexagone
+    /// </summary>
+    /// <param name="centerX"></param>
+    /// <param name="centerY"></param>
+    /// <returns></returns>
+    int GetHexagonTerrainType(int centerX, int centerY)
+    {
+        // Rayon approximatif de l'hexagone
+        int radius = HexWidth / 2;
+        Dictionary<int, int> terrainCount = new Dictionary<int, int>();
+
+        for (int x = centerX - radius; x <= centerX + radius; x++)
+        {
+            for (int y = centerY - radius; y <= centerY + radius; y++)
+            {
+                if (x >= 0 && x < MapTexture.width && y >= 0 && y < MapTexture.height)
+                {
+                    if (IsPixelInHexagon(x,y,centerX,centerY,radius))
+                    {
+                        Color pixelColor = MapTexture.GetPixel(x, y);
+                        int terrainType = GetTerrainTypeFromColor(pixelColor);
+                        if (terrainType != -1)
+                        {
+                            if (!terrainCount.ContainsKey(terrainType))
+                            {
+                                terrainCount[terrainType] = 0;
+                            }
+                            terrainCount[terrainType]++;
+                        }
+                    }                   
+                }
+            }
+        }
+        if (terrainCount.Count == 0)
+            return 1;
+
+        // Retourne la valeur la plus fréquente pour cet hexagone
+        return terrainCount.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+    }
+
+    /// <summary>
+    /// Check is a pixel is in the Hexagone
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="centerX"></param>
+    /// <param name="centerY"></param>
+    /// <param name="hexRadius"></param>
+    /// <returns></returns>
+    bool IsPixelInHexagon(int x, int y, int centerX, int centerY, int hexRadius)
+    {
+        float dx = Math.Abs(x - centerX) / (float)hexRadius;
+        float dy = Math.Abs(y - centerY) / (float)hexRadius;
+
+        return (dx + dy * 2.0f / 3.0f <= 0.95f);
+    }
+
+
+    /// <summary>
+    /// Instantiate a Cell in the grid that Correspond to an int
+    /// </summary>
+    /// <param name="typeOfCell"></param>
+    private void SetCell(int typeOfCell,int x,int y)
     {
         switch (typeOfCell)
         {
-            case 0: Debug.Log("ocean");
+            case 0: instantiateCell(x,y); Debug.Log("ocean");
                 break;
             case 1:
                 Debug.Log("plaine");
@@ -108,27 +193,45 @@ public class MapGenerator : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// Return an int with the corresponding color
     /// </summary>
     /// <param name="color"></param>
     /// <returns></returns>
     private int GetTerrainTypeFromColor(Color color)
     {
-        switch (color)
+        //switch (color)
+        //{
+        //    case Color c when c == MapColors[0]: return 0;// ocean
+        //    case Color c when c == MapColors[1]: return 1;// plaine
+        //    case Color c when c == MapColors[2]: return 2;// forest
+        //    case Color c when c == MapColors[3]: return 3;// montagne       
+        //    case Color c when c == MapColors[4]: return 4;// snow
+        //    case Color c when c == MapColors[5]: return 5;// structure
+        //    case Color c when c == MapColors[6]: return 6;// lac or river
+        //    case Color c when c == MapColors[7]: return 7;// beach
+        //    case Color c when c == MapColors[8]: return 8;// swamp
+        //    default: return -1;// unknow terrain default
+        //}
+        for (int i = 0; i < MapColors.Count; i++)
         {
-            case Color c when c == MapColors[0]: return 0;// ocean
-            case Color c when c == MapColors[1]: return 1;// plaine
-            case Color c when c == MapColors[2]: return 2;// forest
-            case Color c when c == MapColors[3]: return 3;// montagne       
-            case Color c when c == MapColors[4]: return 4;// snow
-            case Color c when c == MapColors[5]: return 5;// structure
-            case Color c when c == MapColors[6]: return 6;// lac or river
-            case Color c when c == MapColors[7]: return 7;// beach
-            case Color c when c == MapColors[8]: return 8;// swamp
-            default: return -1;// unknow terrain default
+            if (AreColorsSimilar(color, MapColors[i]))
+            {
+                return i; // Retourne l'index correspondant au terrain
+            }
         }
+        return -1; // Terrain inconnu si aucune correspondance trouvée
     }
-    
+
+    /// <summary>
+    /// Compares two colors with a certain tolerance
+    /// </summary>
+    private bool AreColorsSimilar(Color c1, Color c2, float tolerance = 0.1f)
+    {
+        return Mathf.Abs(c1.r - c2.r) < tolerance &&
+               Mathf.Abs(c1.g - c2.g) < tolerance &&
+               Mathf.Abs(c1.b - c2.b) < tolerance;
+    }
+
     /// <summary>
     /// Return the position of an hexagonal cell on the map
     /// </summary>
@@ -146,82 +249,4 @@ public class MapGenerator : MonoBehaviour
         float worldY = (UnityEngine.Random.Range(0f, 1f));
         return new Vector3(worldX, worldY, worldZ);
     }
-
-    int[,] DownsampleTerrainMap(int[,] originalMap, int blockSize)
-    {
-        int originalWidth = originalMap.GetLength(0);
-        int originalHeight = originalMap.GetLength(1);
-
-        int newWidth = originalWidth / blockSize;
-        int newHeight = originalHeight / blockSize;
-
-        int[,] downsampledMap = new int[newWidth, newHeight];
-
-        for (int x = 0; x < newWidth; x++)
-        {
-            for (int y = 0; y < newHeight; y++)
-            {
-                downsampledMap[x, y] = GetDominantTerrainType(originalMap, x * blockSize, y * blockSize, blockSize);
-            }
-        }
-
-        return downsampledMap;
-    }
-
-    // Fonction pour obtenir la valeur dominante dans un bloc de pixels
-    int GetDominantTerrainType(int[,] map, int startX, int startY, int blockSize)
-    {
-        Dictionary<int, int> terrainCount = new Dictionary<int, int>();
-
-        for (int x = startX; x < startX + blockSize; x++)
-        {
-            for (int y = startY; y < startY + blockSize; y++)
-            {
-                int terrainType = map[x, y];
-                if (!terrainCount.ContainsKey(terrainType))
-                {
-                    terrainCount[terrainType] = 0;
-                }
-                terrainCount[terrainType]++;
-            }
-        }
-
-        // Retourne la valeur avec la fréquence la plus élevée
-        return terrainCount.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-    }
-
-
-    /*void GenerateMap() //old
-    {
-
-        //Tilemap.size = new Vector3Int (20, 20,20);
-
-        Debug.Log(Tilemap.size);
-        //Tilemap.FloodFill(Vector3Int.zero,new Tile() { gameObject = CellPrefab});
-        for (int y = 0; y < GridHeight; y++)
-        {
-            for (int x = 0; x < GridWidth; x++)
-            {
-                //Vector3Int positionI= new Vector3Int(x, y,0);
-
-                Vector3 position = HexPositioning(x, y, CellSize);
-                //position = SnapCoordinateToGrid(position);
-                GameObject mCell = Instantiate(CellPrefab, position, Quaternion.identity);
-                mCell.transform.parent = hexaGrid.transform;
-                //mCell.transform.parent = Tilemap.transform;
-                //Tile tile = new Tile();
-                //tile.gameObject = CellPrefab;
-                //Tilemap.SetTile(positionI, tile);
-                //tile.gameObject.GetComponent<Cell>().ChangeHeight(MapMaxHeight);
-            }
-        }
-    }*/
-
-
-    //public Vector3 SnapCoordinateToGrid(Vector3 position)
-    //{
-    //    Vector3Int cellPos = gridLayout.WorldToCell(position);
-    //    position = grid.GetCellCenterWorld(cellPos);
-    //    return position;
-    //}
 }
